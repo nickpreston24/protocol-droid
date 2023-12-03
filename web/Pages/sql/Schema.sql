fbbf17853708c66e4beb3d87e5fffe2577911980
mikepreston17 <michael.n.preston@gmail.com>
2023-12-03 14:50:57 -0600

    updated packages and lang version

--- web/Pages/sql/Schema.sql
+++ web/Pages/sql/Schema.sql
@@ -1,16 +1,16 @@
 drop table regex_patterns; -- if not exists regex_patterns
 CREATE TABLE regex_patterns
 (
-    id                serial primary key,
-    name              varchar(40) UNIQUE,
-    find              varchar(250),
-    replacement       varchar(250),
-    languages         varchar(150),
-    extensions        varchar(150),
-    target_extensions varchar(150),
-    target_language   varchar(150),
-    description       varchar(150),
-    enabled           boolean default false
+    -- id                serial primary key,
+    -- name              varchar(40) UNIQUE,
+    -- find              varchar(250),
+    -- replacement       varchar(250),
+    -- languages         varchar(150),
+    -- extensions        varchar(150),
+    -- target_extensions varchar(150),
+    -- target_language   varchar(150),
+    -- description       varchar(150),
+    -- enabled           boolean default false
 );
 
 drop table cars;
@@ -20,3 +20,5 @@ CREATE TABLE cars
     name  VARCHAR(255),
     price INT
 )
+
+-- https://ammoseek.com/ammo/300aac-blackout/Fiocchi?sort=mfg
